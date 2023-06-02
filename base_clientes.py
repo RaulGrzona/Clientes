e21df07af550d3dc44be27989824dcdf0084308f
RaulGrzona <raulgrzona@yahoo.com>
2023-06-02 14:44:55 -0300

    Se crea .gitignore base_clientes.py y requirements.txt

---
+++ base_clientes.py
@@ -0,0 +1,6 @@
+class Clientes:
+    def _init_(self, nonbre, telefono, direccion, iva):
+        self.nonbre     = nonbre
+        self.telefono   = telefono
+        self.direccion  = direccion
+        self.iva        = iva
